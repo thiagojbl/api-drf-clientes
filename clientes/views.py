fb7e713491097babb573952d2d96c5f548816bfc
thiago jose <thiagojb12@gmail.com>
2022-08-23 09:29:08 -0300

    Deploy

--- clientes/views.py
+++ clientes/views.py
@@ -9,5 +9,9 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend,
+                       filters.OrderingFilter,
+                       filters.SearchFilter]
     ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']
+    filterset_fields = ['ativo']
--- setup/settings.py
+++ setup/settings.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -122,3 +124,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
 }
+
+django_heroku.settings(locals())
